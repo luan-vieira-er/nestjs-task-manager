ee4e6b2c39da631bdbf07c536bbe10fc3b4f5b75
Luan Vieira <luanvieira.er@hotmail.com>
2023-08-30 19:40:11 -0300

    Section 4: Persistence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,43 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Task } from './tasks/task.entity';
+import { DataSource } from 'typeorm';
 
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'task-management',
+  autoLoadEntities: true, 
+  synchronize: true,
+  entities: [__dirname + '../**/*.entity{.ts,.js}']
+}
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'task-management',
+  synchronize: true,
+  entities: [__dirname + '../**/*.entity{.ts,.js}']
+});
+
+
+let dataSource: DataSource;
+
+export const ConnectDb = async () => {
+    dataSource = await AppDataSource.initialize();
+}
+    
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule
+  ],
 })
 export class AppModule {}
+
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,40 +1,38 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
 
     @Get()
-    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-        if (Object.keys(filterDto).length){
-            return this.tasksService.getTasksWithFilters(filterDto)
-        }
-        return this.tasksService.getAllTasks()
+    getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.tasksService.getTasks(filterDto)
     }
 
     @Get('/:id')
-    getTaskById(@Param('id') id: string): Task {
+    getTaskById(@Param('id') id: string): Promise<Task> {
         return this.tasksService.getTaskById(id)
     }
 
     @Delete('/:id')
-    deleteTask(@Param('id') id: string): void {
+    deleteTask(@Param('id') id: string): Promise<void> {
         return this.tasksService.deleteTask(id)
     }
 
     @Patch('/:id/status')
-    updateTask(@Param('id') id: string, @Body() updateTaskStatusDto: UpdateTaskStatusDto): Task {
+    updateTask(@Param('id') id: string, @Body() updateTaskStatusDto: UpdateTaskStatusDto): Promise<Task> {
         const { status } = updateTaskStatusDto
         return this.tasksService.updateTask(id, status)
     }
 
     @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
         return this.tasksService.createTask(createTaskDto)
     }
 }
